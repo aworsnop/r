3e7a4ea5bb61e0ff8ce6eec7a7f5b4760fa52f0f
aworsnop <aworsnop@users.noreply.github.com>
2023-10-21 11:10:33 -0400

    Update blah.js

--- blah.js
+++ blah.js
@@ -1,7 +1,11 @@
-document.querySelector("#tltbt > div.Nl > div.Ll.d.xq.ef.sm.um").remove();document.querySelector("#tl_ > div.wf > div > form > div.Zj > input").setAttribute('hidden', 'hidden');
+document.querySelector("#tltbt > div.Nl").remove();
+document.querySelector("#tl_ > div.wf > div > form").remove();
+
+
+/* document.querySelector("#tltbt > div.Nl > div.Ll.d.xq.ef.sm.um").remove();document.querySelector("#tl_ > div.wf > div > form > div.Zj > input").setAttribute('hidden', 'hidden');
 
 document.querySelector("#tl_ > div.wf > div > form > div.bb.Vj.Xj.Wj").setAttribute('hidden', 'hidden')
 
-/* document.querySelector("#tl_ > /*div.wf > div > form > div.Zj").remove();document.querySelector("#tl_ > div.wf > div > form").remove();*/
+document.querySelector("#tl_ > /*div.wf > div > form > div.Zj").remove();document.querySelector("#tl_ > div.wf > div > form").remove();*/
 
 
