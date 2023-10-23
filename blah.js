06aa9b4800ff0fd201922f54e0939ee0b2590c6c
aworsnop <aworsnop@users.noreply.github.com>
2023-10-23 17:49:12 -0400

    Update blah.js

--- blah.js
+++ blah.js
@@ -2,10 +2,14 @@ document.querySelector("#tltbt > div.Nl").remove();
 document.querySelector("#tl_ > div.wf > div > form > div.Zj > input").setAttribute('hidden', 'hidden');
 document.querySelector("#tl_ > div.wf > div > form > div.bb.Vj.Xj.Wj").setAttribute('hidden', 'hidden');
 document.querySelector("#tl_ > div.wf > div > form.bg").style.height = "0px";
-document.querySelector("#tltbt > div.wm > div > div").setAttribute('hidden', 'hidden');
+document.querySelector("#tltbt > div.wm > div > div.mf").textContent=document.querySelector("#tl_ > div.wf > div > form > div.Zj > input.Pj.ak").value
 
 
-/* document.querySelector("#tl_ > div.wf > div > form").remove();
+
+
+/* document.querySelector("#tltbt > div.wm > div > div").setAttribute('hidden', 'hidden');
+
+document.querySelector("#tl_ > div.wf > div > form").remove();
  document.querySelector("#tltbt > div.Nl > div.Ll.d.xq.ef.sm.um").remove();document.querySelector("#tl_ > div.wf > div > form > div.Zj > input").setAttribute('hidden', 'hidden');
 
 document.querySelector("#tl_ > div.wf > div > form > div.bb.Vj.Xj.Wj").setAttribute('hidden', 'hidden')
